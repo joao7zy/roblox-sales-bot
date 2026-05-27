92d6dfd18259b9a2f34231f6da49632f46bbe9a5
joao7zy <plugraro5@gmail.com>
2026-05-27 19:15:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -857,7 +857,7 @@ async function bootstrapTodayStats() {
   const sales = await getSales();
 
   for (const tx of [...sales].reverse()) {
-    await processTransaction(tx, true, true);
+    await processTransaction(tx, false, true);
   }
 
   meta.bootstrappedDay = todayKey;
@@ -872,7 +872,7 @@ async function checkSales() {
   if (!sales.length) return;
 
   for (const tx of [...sales].reverse()) {
-    await processTransaction(tx, false, false);
+    await processTransaction(tx, true, false);
   }
 }
 
